8f53db4a0e78e7833e1e18fe594e6454fa0d74ce
Abdelghanikemmissa <abdokemmissa@gmail.com>
2025-05-06 14:50:53 +0100

    Project C++ ended

--- headers/score.hpp
+++ headers/score.hpp
@@ -1,21 +1,3 @@
-// #ifndef SCORE_HPP
-// #define SCORE_HPP
-
-// #include <SDL2/SDL.h>
-
-// class Score {
-// public:
-//     Score();
-//     void increase(int amount);
-//     void reset();
-//     int getScore() const;
-//     void render(SDL_Renderer* renderer, int x, int y);
-
-// private:
-//     int score;
-// };
-
-// #endif
 #ifndef SCORE_HPP
 #define SCORE_HPP
 
