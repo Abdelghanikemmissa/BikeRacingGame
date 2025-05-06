4159a5e46e02d7b49a9ff1d99a8ae9bae6dc5f66
Abdelghanikemmissa <abdokemmissa@gmail.com>
2025-05-06 11:20:02 +0100

    C++

--- headers/score.hpp
+++ headers/score.hpp
@@ -1,46 +1,18 @@
 #ifndef SCORE_HPP
 #define SCORE_HPP
 
-#include <string>
-#include <vector>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
 
-// Structure pour stocker un score
-struct ScoreEntry {
-    std::string playerName;
-    int score;
-    float time;
-    
-    ScoreEntry(const std::string& name, int scr, float t) 
-        : playerName(name), score(scr), time(t) {}
-};
-
-class ScoreManager {
+class Score {
 public:
-    static const int MAX_SCORES = 10;
-    static const char* SCORES_FILE;
-
-    ScoreManager();
-    ~ScoreManager();
-
-    // Gestion des scores
-    void addScore(const std::string& playerName, int score, float time);
-    std::vector<ScoreEntry> getHighScores() const;
-    void saveScores() const;
-    void loadScores();
-
-    // Affichage
-    void renderScores(SDL_Renderer* renderer, TTF_Font* font, int x, int y) const;
-    void renderScore(SDL_Renderer* renderer, TTF_Font* font, int score, int x, int y) const;
-
-    // Vérification si un score est dans les highscores
-    bool isHighScore(int score) const;
+    Score();
+    void increase(int amount);
+    void reset();
+    int getScore() const;
+    void render(SDL_Renderer* renderer, int x, int y);
 
 private:
-    std::vector<ScoreEntry> highScores;
-
-    void sortScores();
+    int score;
 };
 
-#endif // SCORE_HPP
+#endif
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+int Cars::getcouts()
+{
+    return count;
+}
